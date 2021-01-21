50fad241312b3ff896e302feef34fce8cebc6ccc
guneycansanli <48263266+guneycansanli@users.noreply.github.com>
2021-01-21 21:12:16 +0300

    Fixed Errors and now we can reset our password....

--- blog_app/js/index.js
+++ blog_app/js/index.js
@@ -77,4 +77,32 @@ var firebaseConfig = {
   {
     firebase.auth().signOut();
   });
+
+  $("#btn-resetPassword").click(function()
+  {
+    var auth =  firebase.auth();
+    var email = $("#email").val();
+
+    if(email != "")
+    {
+      auth.sendPasswordResetEmail(email).then(function()
+      {
+        window.alert("Email has been sent to you please check and verify..");
+      })
+      .catch(function(error)
+      {
+        var errorCode = error.code;
+        var errorMessage = error.message;
+
+        console.log(errorCode);
+        console.log(errorMessage);
+
+        window.alert("Message : "+ errorMessage);
+      });
+    }
+    else
+    {
+      window.alert("Please write your e-mail firstly.");
+    }
+  });
   
